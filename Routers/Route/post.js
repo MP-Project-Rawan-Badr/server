b00e9ecebd5c132d533843267bf7d1e426d73c73
Brawan89 <Rawan.Badr.elq.js@tuwaiq.edu.sa>
2021-12-23 11:22:26 +0300

    update post controller

--- Routers/Route/post.js
+++ Routers/Route/post.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const postRouter = express.Router();
-const {addPost , getAllPosts} = require("./../Controller/post");
+const {addPost , getAllPosts } = require("./../Controller/post");
 
 
 //
 postRouter.post("/addpost", addPost);
 postRouter.get("/getAllPosts",  getAllPosts);
+
 
 
 module.exports = postRouter;
