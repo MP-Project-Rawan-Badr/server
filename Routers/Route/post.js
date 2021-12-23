9d9db645c241226e69aa3541845e9ec4e4610666
Brawan89 <Rawan.Badr.elq.js@tuwaiq.edu.sa>
2021-12-23 11:13:41 +0300

    get all  posts route

--- Routers/Route/post.js
+++ Routers/Route/post.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const postRouter = express.Router();
-const {addPost } = require("./../Controller/post");
+const {addPost , getAllPosts} = require("./../Controller/post");
 
 
 //
 postRouter.post("/addpost", addPost);
+postRouter.get("/getAllPosts",  getAllPosts);
 
 
 module.exports = postRouter;
