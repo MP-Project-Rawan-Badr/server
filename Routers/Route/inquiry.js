e5f2810e08324dea73f708d528ca0654a0e22163
Brawan89 <Rawan.Badr.elq.js@tuwaiq.edu.sa>
2021-12-23 14:37:55 +0300

    delete inquiry route

--- Routers/Route/inquiry.js
+++ Routers/Route/inquiry.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const inquiryRouter = express.Router();
-const {addinquiry, getInquiries, updateInquiry} = require("./../Controller/inquiry");
+const {addinquiry, getInquiries, updateInquiry , deleteInquiry} = require("./../Controller/inquiry");
 
 
-//
 inquiryRouter.post("/addinquiry", addinquiry);
 inquiryRouter.get("/getInquiries", getInquiries);
 inquiryRouter.put("/updateInquiry/:id", updateInquiry);
-
+inquiryRouter.put("/deleteInquiry/:id", deleteInquiry);
 
 module.exports = inquiryRouter;
